c62592ac2643959e0af9c64fd3acfa803e6bdc72
Rose Maina <rosewairimu27@gmail.com>
2022-09-29 23:12:54 +0300

    commit tree f21bf4df209df78ac527af31f0d59c93bcef6398

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+    # returns a user with an array of all items associated with that user
+        def show
+        users = User.find(params[:id])
+        render json: users, include: :items
+        end
 end
